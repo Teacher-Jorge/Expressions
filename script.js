e8834a83f518eafdaa5c86092b16f6055740b445
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2022-07-31 15:10:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const btnPlay = document.getElementById('btnPlay')
 const btnPause = document.getElementById('btnPause')
 const btnRepeat = document.getElementById('btnRepeat')
+const btnOnce = document.getElementById('btnOnce')
+const onceIn = document.getElementById('onceIn')
+
+btnOnce.addEventListener('click', function() {
+    const audio = document.getElementById('click')
+    audio.play()
+    onceIn.textContent = 'Uma vez na lua azul. - (Quase nunca)'
+    onceIn.style.color = 'darkviolet'
+    btnOnce.style.color = 'lime'
+    setTimeout(function() {
+        onceIn.textContent = 'Once in a blue moon.'
+        onceIn.style.color = 'black'
+        btnOnce.style.color = 'fuchsia'
+    },1500)
+})
 
 btnPlay.addEventListener('click', function() {
     const audio = document.getElementById('once')
