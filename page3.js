9cc5452852e906c77f3d19ec1daba8477d3f7e7f
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2022-08-01 14:33:04 -0300

    Add files via upload

--- page3.js
+++ page3.js
@@ -1,45 +1,42 @@
-//EXPRESSION 01
+//EXPRESSION 02
 let btnPlay = document.getElementById('btnPlay')
 let btnPause = document.getElementById('btnPause')
 let btnRepeat = document.getElementById('btnRepeat')
 let btnTranslate = document.getElementById('btnTranslate')
-let onceIn = document.getElementById('onceIn')
+let swissCheese = document.getElementById('swissCheese')
 
 btnTranslate.addEventListener('click', function() {
     let audio = document.getElementById('click')
     audio.play()
-    onceIn.textContent = 'Na verdade eu só vou ao cinema uma vez na lua azul. - (Quase nunca.)'
-    onceIn.style.color = 'darkviolet'
+    swissCheese.textContent = 'A hitória tem mais buracos que um queijo suiço. - (...tem muitas falhas.)'
+    swissCheese.style.color = 'darkviolet'
     btnTranslate.style.color = 'red'
     setTimeout(function() {
-        onceIn.textContent = 'I actually just go to the movies once in a blue moon.'
-        onceIn.style.color = 'black'
+        swissCheese.textContent = 'The plot has more holes than a Swiss cheese.'
+        swissCheese.style.color = 'black'
         btnTranslate.style.color = 'lime'
     },4000)
 })
 
 btnPlay.addEventListener('click', function() {
-    let audio = document.getElementById('once')
+    let audio = document.getElementById('swiss')
     audio.play()
     btnPlay.style.color = 'lime'
     btnPause.style.color = 'dodgerblue'
     btnRepeat.style.color = 'dodgerblue'
 })
 btnPause.addEventListener('click', function() {
-    let audio = document.getElementById('once')
+    let audio = document.getElementById('swiss')
     audio.pause()
     btnPause.style.color = 'lime'
     btnPlay.style.color = 'dodgerblue'
     btnRepeat.style.color = 'dodgerblue'
 })
 btnRepeat.addEventListener('click', function() {
-    let audio = document.getElementById('once')
+    let audio = document.getElementById('swiss')
     audio.currentTime = 0
     audio.play()
     btnRepeat.style.color = 'red'
     btnPlay.style.color = 'lime'
     btnPause.style.color = 'dodgerblue'
 })
-
-
-
